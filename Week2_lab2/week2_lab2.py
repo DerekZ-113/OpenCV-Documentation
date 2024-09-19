5d751d7e933ae712b3d2388347261c3e8613eba4
Derek Zhang <zderek113@gmail.com>
2024-09-19 14:36:22 -0700

    week 3 lab 3

--- Week2_lab2/week2_lab2.py
+++ Week2_lab2/week2_lab2.py
@@ -2,14 +2,35 @@ import cv2
 
 img = cv2.imread("flower.png")
 
-gray_img = cv2.imread("flower.png", cv2.IMREAD_GRAYSCALE)
+# gray_img = cv2.imread("flower.png", cv2.IMREAD_GRAYSCALE)
 
-cv2.imshow("flower_image", img)
-
+# week 2 codes
+# cv2.imshow("flower_image", img)
 # cv2.waitKey(0)
-
 # cv2.waitKey(500)
-
 # cv2.destroyAllWindows()
+# cv2.imwrite("gray_flower.png", gray_img)
 
-cv2.imwrite("gray_flower.png", gray_img)
+# week 3 codes
+#resize
+resized_img = cv2.resize(img, (300, 300))
+cv2.imshow("resized image", resized_img)
+
+#crop
+cropped_img = img[80:200, 150, 330]
+cv2.imshow("cropped image", cropped_img)
+
+#rotate
+rotated_img = img(img, cv2.ROTATE_90_CLOCKWISE)
+cv2.imshow("rotated image", rotated_img)
+
+#gray scale and HSV
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+#draw shapes and text-line
+print(img.shape) # to get height, width, channels of the image
+cv2.rectangle(img, (100, 100), (200, 200), (255, 0, 0), 3)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ Week3_lab3/README.md
@@ -0,0 +1,7 @@
+## Week 3 OpenCV lab 3
+
+# Basics of Images 
+## Resize, crop, rotate, gray scale, HSV, and shapes
+
+### Resize
+- syntax: cv2.resize(image, (width, height), fx, fy, interpolation)
